2406b3f50ceea2502ef1513b812a0c72208c2dee
Matteo Mastropasqua <teo.mastropasqua@gmail.com>
2018-01-22 16:37:36 +0100

    no message

---
+++ Assets/Script/Grid.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+    public GameObject casella;
+    GameObject[,] grid;
+
+	void Start () {
+
+        // Crea una griglia (X x Z)
+        gridSize(10,10);
+
+	}
+	
+
+
+    void gridSize(int x, int z)
+    {
+        // Crea un array grande quanto la griglia
+        grid = new GameObject[x,z];
+
+        // Crea cloni delle caselle per formare la griglia
+        for (int i = 0; i < x; i++)
+        {
+            for (int n = 0; n < z; n++)
+            {                
+                GameObject gridPlane = Instantiate(casella);
+                gridPlane.transform.position += new Vector3(0.50038f * i, 0, 0.50038f * n);                
+            }
+        }
+    }
+}
