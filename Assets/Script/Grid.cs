d8b82a6db5ab993f6182de6941da31323e788387
Matteo Mastropasqua <teo.mastropasqua@gmail.com>
2018-01-24 14:45:36 +0100

    Creazione griglia + prototipo movimento pedina

--- Assets/Script/Grid.cs
+++ Assets/Script/Grid.cs
@@ -4,31 +4,88 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour {
 
+    public Transform selectedCasella;
     public GameObject casella;
-    GameObject[,] grid;
+    public GameObject[,] grid;
 
-	void Start () {
+    int xAxis = 0;
+    int zAxis = 0;
+    public int selectedX = 0;
+    public int selectedZ = 0;
+    void Start () {
 
         // Crea una griglia (X x Z)
-        gridSize(10,10);
-
+        GridSize(10,3);
+        selectedCasella = grid[0, 0].transform;
+        
 	}
-	
 
+    private void Update()
+    {
+        
+        // Elimina una casella
+        Select();
+        
+    }
 
-    void gridSize(int x, int z)
+    void GridSize(int x, int z)
     {
         // Crea un array grande quanto la griglia
         grid = new GameObject[x,z];
 
         // Crea cloni delle caselle per formare la griglia
-        for (int i = 0; i < x; i++)
+        for (int X = 0; X < x; X++)
         {
-            for (int n = 0; n < z; n++)
-            {                
-                GameObject gridPlane = Instantiate(casella);
-                gridPlane.transform.position += new Vector3(0.50038f * i, 0, 0.50038f * n);                
+            for (int Z = 0; Z < z; Z++)
+            {
+                GameObject casellaClone = Instantiate(casella);
+                Destroy(casellaClone.GetComponent<Grid>());
+                casellaClone.transform.position += new Vector3(casella.transform.localScale.x * X, 0,casella.transform.localScale.z * Z);
+                // Assegna le caselle alla Griglia
+                grid[X,Z] = casellaClone;
+                xAxis = X;
+                zAxis = Z;
             }
+            Destroy(gameObject.GetComponent<MeshRenderer>());
         }
+        
+    }
+
+
+
+
+
+    void Select()
+    {
+        
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) && selectedX < xAxis)
+        {
+            selectedX++;
+            Debug.Log("x = " + selectedX);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && selectedX > 0)
+        {
+            selectedX--;
+            Debug.Log("x = " + selectedX);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && selectedZ < zAxis)
+        {
+            selectedZ++;
+            Debug.Log(selectedZ);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) && selectedZ > 0)
+        {
+            selectedZ--;
+            Debug.Log(selectedZ);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Destroy(grid[selectedX,selectedZ].GetComponent<MeshRenderer>());
+        }
+
+        selectedCasella = grid[selectedX, selectedZ].transform;
     }
 }
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    Transform myTransform;
+    public GameObject grid;
+
+
+	void Start () {
+        myTransform = GetComponent<Transform>();
+	}
+	
+
+	void Update () {
+        myTransform.position = grid.GetComponent<Grid>().selectedCasella.position;
+        
+
+    }
+}
